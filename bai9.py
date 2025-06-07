016058f7fc6cbf0e2894b2883bef67ed6d153ac5
Mai Duc Phat <duckfast1807@gmail.com>
2025-06-07 20:05:52 +0700

    in so nguyen to

---
+++ bai9.py
@@ -0,0 +1,12 @@
+#in cac so nguyen to tu 1-100
+def prime(n):
+    if n <=1:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if (n%i==0):
+            return False
+    return True
+    
+for i in range(1,101):
+    if (prime(i)):
+        print(i, end = " ")
